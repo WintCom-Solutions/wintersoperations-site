6f2214af4bc489c58b2158a6a14bb082d546f1d5
KarlDavidWinters <karldavidwinters@gmail.com>
2026-08-15 19:00:52 -0500

    Add /about to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -23,5 +23,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.7,
     },
+    {
+      url: `${siteUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
   ];
 }
